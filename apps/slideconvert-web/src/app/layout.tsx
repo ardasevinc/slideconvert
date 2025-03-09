753438bc59a1f34e7432ed61f10c07377438932f
Arda Sevinç <arda@ardasevinc.com>
2025-03-09 23:14:56 +0300

    feat(web): custom blue theme

--- apps/slideconvert-web/src/app/layout.tsx
+++ apps/slideconvert-web/src/app/layout.tsx
@@ -24,10 +24,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang='en'
-      className={`${geistSans.variable} ${geistMono.variable} dark`}
-    >
+    <html lang='en' className={`${geistSans.variable} ${geistMono.variable}`}>
       <body className='antialiased'>{children}</body>
     </html>
   );
