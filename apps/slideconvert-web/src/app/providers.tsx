2cdad0fb1348f6b0b54491449765ab7a5ebca281
Arda Sevinç <arda@ardasevinc.com>
2025-03-12 14:32:36 +0300

    refactor(web): react query should be init inside component

--- apps/slideconvert-web/src/app/providers.tsx
+++ apps/slideconvert-web/src/app/providers.tsx
@@ -2,10 +2,20 @@
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Toaster } from '@/components/ui/sonner';
-
-const queryClient = new QueryClient();
+import { useState } from 'react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
